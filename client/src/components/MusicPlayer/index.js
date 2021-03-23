61bf832a5e166b9f03dd7e3d4ac6e39f2c862787
Gavin Phill <carpe.gavin@gmail.com>
2021-03-23 18:42:54 -0500

    musicplayer component work

--- client/src/components/MusicPlayer/index.js
+++ client/src/components/MusicPlayer/index.js
@@ -12,3 +12,21 @@ class ResponsivePlayer extends Component {
       )
     }
   }
+
+//--------------------------non-responsive below--------------------------
+//   function App() {
+//     return (
+//       <div>
+//         <h3>Audio player in React - <a href="https://www.cluemediator.com">Clue Mediator</a></h3>
+//         <ReactPlayer
+//           url="https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_700KB.mp3"
+//           width="400px"
+//           height="50px"
+//           playing={false}
+//           controls={true}
+//         />
+//       </div>
+//     );
+//   }
+   
+//   export default App;
