49537f10cd7984a1f8b2e28c720f0bacdd13be2f
Devante W <williams.devante05@gmail.com>
2021-03-27 12:28:11 -0500

    fix home page

--- client/src/components/pages/Home/index.js
+++ client/src/components/pages/Home/index.js
@@ -10,37 +10,6 @@ function Home() {
 
 
     <>
-        <nav className='navbar'>
-          <div className='navbar-container container'>
-            <Link to='/' className='navbar-logo' id="nameHead" onClick={closeMobileMenu}>
-              
-              WePoo
-                          </Link>
-
-            <ul className={ 'nav-menu active' }>
-              <li className='nav-item'>
-                <Link to='/map' className='nav-links' onClick={closeMobileMenu}>
-                  Gotta Go
-                </Link>
-              </li>
-              <li className='nav-item'>
-                <Link
-                  to='/player'
-                  className='nav-links'
-                  onClick={closeMobileMenu}
-                >
-                  Going
-                </Link>
-              </li>
-              <li className='nav-item'>
-                <Link
-                  to='/review'
-                  className='nav-links'
-                  onClick={closeMobileMenu}
-                >
-                  Gone
-                </Link>
-              </li>
 
     <Navbar/>
     <Footer/>
