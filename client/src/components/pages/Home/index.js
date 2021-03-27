8fabccac7912b5a3353017608ce1b51fd9f25e29
Devante W <williams.devante05@gmail.com>
2021-03-27 12:08:47 -0500

    add homescreen icons and fix color scheme

--- client/src/components/pages/Home/index.js
+++ client/src/components/pages/Home/index.js
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
 import Navbar from "../../Navbar"
+import Footer from "../../Footer"
 import './Home.css';
 
 
 function Home() {
 
   return (
+    <>
     <Navbar/>
+    <Footer/>
     
-
+    </>
   );
 }
 
