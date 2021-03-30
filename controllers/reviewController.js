3aa51f21499ff3b329bafbcf391f36910ebe72b5
Devante W <williams.devante05@gmail.com>
2021-03-29 19:15:06 -0500

    create orm and routes for reviews

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,34 +1,33 @@
 const db = require("../models");
 
-// Defining methods for the booksController
 module.exports = {
   findAll: function(req, res) {
-    db.Book
+    db.Review
       .find(req.query)
       .sort({ date: -1 })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   findById: function(req, res) {
-    db.Book
+    db.Review
       .findById(req.params.id)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   create: function(req, res) {
-    db.Book
+    db.Review
       .create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   update: function(req, res) {
-    db.Book
+    db.Review
       .findOneAndUpdate({ _id: req.params.id }, req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   remove: function(req, res) {
-    db.Book
+    db.Review
       .findById({ _id: req.params.id })
       .then(dbModel => dbModel.remove())
       .then(dbModel => res.json(dbModel))
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
-const bookRoutes = require("./books");
+const reviewRoutes = require("./reviews");
 
 // Book routes
-router.use("/books", bookRoutes);
+router.use("/review", reviewRoutes);
 
 module.exports = router;
