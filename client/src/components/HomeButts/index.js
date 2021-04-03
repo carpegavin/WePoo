d9a71ded7667d2e6a7cc6b68c4bd5eba45eb83b5
Gavin Phill <carpe.gavin@gmail.com>
2021-04-03 11:30:33 -0500

    button work

--- client/src/components/HomeButts/index.js
+++ client/src/components/HomeButts/index.js
@@ -3,40 +3,37 @@ import { Button } from "react-bootstrap";
 // import { Link } from "react-router-dom";
 import "./butts.css";
 // import './Navbar.css';
-import { Row, Container, Col } from "react-bootstrap";
+// import { Row, Container, Col } from "react-bootstrap";
+// import { styles } from "react-bootstrap";
 
 class HomeButton extends Component {
   render() {
     return (
       <>
-      <grid class="all">
-        <row className="text-center">
-          <div class="row align-items-center h-100">
-            <div class="col-md-6 col-md-offset-3">
-              <Button variant="warning" size="lg" onClick={() => "/map"}>
-                GOTTA GO
-              </Button>
-            </div>
+        
+            <div class="butts vertical-center">
+              <div className="align-baseline" >
+                <Button variant="warning" size="lg" onClick={() => "/map"}>
+                  GOTTA GO
+                </Button>
+              </div>
+            
 
-            <div class="col-md-6 col-md-offset-3">
-              <Button variant="warning" size="lg" onClick={() => "/player"}>
-                GOING
-              </Button>
-            </div>
+              <div className="align-baseline">
+                <Button variant="warning" size="lg" onClick={() => "/player"}>
+                  GOING
+                </Button>
+              </div>
 
-            <div class="col-md-6 col-md-offset-3">
-              <Button
-                fixed="center"
-                variant="warning"
-                size="lg"
-                onClick={() => "/review"}
-              >
-                GONE
-              </Button>
+              <div className="align-baseline" >
+                <Button variant="warning" size="lg" onClick={() => "/review"}>
+                  GONE
+                </Button>
+              </div>
             </div>
-          </div>
-        </row>
-      </grid>
+          
+            
+          
       </>
     );
   }
