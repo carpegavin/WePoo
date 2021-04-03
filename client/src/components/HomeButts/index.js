c0a2c56981f3ae40de0f417abdf48d0124db006d
Gavin Phill <carpe.gavin@gmail.com>
2021-04-03 13:11:01 -0500

    button work

--- client/src/components/HomeButts/index.js
+++ client/src/components/HomeButts/index.js
@@ -3,26 +3,32 @@ import { Button } from "react-bootstrap";
 // import { Link } from "react-router-dom";
 import "./butts.css";
 // import './Navbar.css';
-import { Row, Container, Col, Span, ButtonGroup } from "react-bootstrap";
+import { Row, Container, Col, Span, ButtonGroup,  } from "react-bootstrap";
 // import { styles } from "react-bootstrap";
 
 class HomeButton extends Component {
   render() {
     return (
-      <div className="container" style={{ textAlign: "center" }}>
-        <ButtonGroup vertical>
+      <div className = "container" style={{textAlign:"center"}}>
+        <ButtonGroup vertical >
           <div class="butts vertical-center">
-            <Button variant="warning" size="lg" onClick={() => "/map"}>
-              GOTTA GO
-            </Button>
+            <Col>
+              <Button variant="warning" size="lg" onClick={() => "/map"}>
+                GOTTA GO
+              </Button>
+            </Col>
 
-            <Button variant="warning" size="lg" onClick={() => "/player"}>
-              GOING
-            </Button>
+            <Col>
+              <Button variant="warning" size="lg" onClick={() => "/player"}>
+                GOING
+              </Button>
+            </Col>
 
-            <Button variant="warning" size="lg" onClick={() => "/review"}>
-              GONE
-            </Button>
+            <Col>
+              <Button variant="warning" size="lg" onClick={() => "/review"}>
+                GONE
+              </Button>
+            </Col>
           </div>
         </ButtonGroup>
       </div>
