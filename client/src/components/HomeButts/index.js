908d30247ecebd76e0c9c09b22745daea5ce446f
Gavin Phill <carpe.gavin@gmail.com>
2021-04-03 12:08:44 -0500

    button work

--- client/src/components/HomeButts/index.js
+++ client/src/components/HomeButts/index.js
@@ -3,37 +3,34 @@ import { Button } from "react-bootstrap";
 // import { Link } from "react-router-dom";
 import "./butts.css";
 // import './Navbar.css';
-// import { Row, Container, Col } from "react-bootstrap";
+import { Row, Container, Col } from "react-bootstrap";
 // import { styles } from "react-bootstrap";
 
 class HomeButton extends Component {
   render() {
     return (
       <>
-        
-            <div class="butts vertical-center">
-              <div className="align-baseline" >
-                <Button variant="warning" size="lg" onClick={() => "/map"}>
-                  GOTTA GO
-                </Button>
-              </div>
-            
+        <div class="btn-group-vertical">
+          <div class="butts vertical-center">
+            <Col>
+              <Button variant="warning" size="lg" onClick={() => "/map"}>
+                GOTTA GO
+              </Button>
+            </Col>
 
-              <div className="align-baseline">
-                <Button variant="warning" size="lg" onClick={() => "/player"}>
-                  GOING
-                </Button>
-              </div>
+            <Col>
+              <Button variant="warning" size="lg" onClick={() => "/player"}>
+                GOING
+              </Button>
+            </Col>
 
-              <div className="align-baseline" >
-                <Button variant="warning" size="lg" onClick={() => "/review"}>
-                  GONE
-                </Button>
-              </div>
-            </div>
-          
-            
-          
+            <Col>
+              <Button variant="warning" size="lg" onClick={() => "/review"}>
+                GONE
+              </Button>
+            </Col>
+          </div>
+        </div>
       </>
     );
   }
