9f51b69e918de99b18c84c29aa69ade0e8435520
Gavin Phill <carpe.gavin@gmail.com>
2021-04-03 13:58:15 -0500

    button work

--- client/src/components/HomeButts/index.js
+++ client/src/components/HomeButts/index.js
@@ -3,34 +3,34 @@ import { Button } from "react-bootstrap";
 // import { Link } from "react-router-dom";
 import "./butts.css";
 // import './Navbar.css';
-import { Row, Container, Col, Span, ButtonGroup,  } from "react-bootstrap";
+import { Row, Container, Col, Span, ButtonGroup } from "react-bootstrap";
 // import { styles } from "react-bootstrap";
 
 class HomeButton extends Component {
   render() {
     return (
-      <div className = "container" style={{textAlign:"center"}}>
-        <ButtonGroup vertical >
+      <div className="container" style={{ textAlign: "center" }}>
+        <div class="btn-group btn-group-sm" vertical >
           <div class="butts vertical-center">
-            <Col>
-              <Button variant="warning" size="lg" onClick={() => "/map"}>
+            <div class="col-md-4">
+              <Button variant="warning"  onClick={() => "/map"}>
                 GOTTA GO
               </Button>
-            </Col>
+            </div>
 
-            <Col>
+            <Col class="col-md-4">
               <Button variant="warning" size="lg" onClick={() => "/player"}>
                 GOING
               </Button>
             </Col>
 
-            <Col>
+            <Col class="buttCol">
               <Button variant="warning" size="lg" onClick={() => "/review"}>
                 GONE
               </Button>
             </Col>
           </div>
-        </ButtonGroup>
+        </div>
       </div>
     );
   }
