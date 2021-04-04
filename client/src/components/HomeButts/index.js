88530e3188d0022c385634de19d184363f59f698
Gavin Phill <carpe.gavin@gmail.com>
2021-04-03 21:36:59 -0500

    button navigation work

--- client/src/components/HomeButts/index.js
+++ client/src/components/HomeButts/index.js
@@ -1,37 +1,33 @@
 import React, { Component } from "react";
 import { Button } from "react-bootstrap";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./butts.css";
+import { props } from "react";
 // import './Navbar.css';
 import { Row, Container, Col, Span, ButtonGroup } from "react-bootstrap";
 // import { styles } from "react-bootstrap";
+// import ""{ useHistory } from 'react-router-dom';
+
+// const history = useHistory();
 
 class HomeButton extends Component {
   render() {
     return (
       <div className="container" style={{ textAlign: "center" }}>
-        <div class="btn-group btn-group-sm" vertical >
+        <div class="btn-group btn-group-sm" vertical>
           <div class="butts vertical-center">
 
-            <Col style={{height: "9.5rem" }}>
-              <Button class="one"
-              onClick={() => "/map"}>
-                GOTTA GO
-              </Button>
+            <Col style={{ height: "9.5rem" }}>
+              <Button onClick={()=> 
+                this.props.history.push('/map')}>GOTTA GO</Button>
             </Col>
 
-            <Col style={{height: "9.5rem"}}>
-              <Button class="two"
-              onClick={() => "/player"}>
-                GOING
-              </Button>
+            <Col style={{ height: "9.5rem" }}>
+              <Button onClick={() => "/player"}>GOING</Button>
             </Col>
 
-            <Col style={{height: "9.5rem"}}>
-              <Button  
-              onClick={() => "/review"}>
-                GONE
-              </Button>
+            <Col style={{ height: "9.5rem" }}>
+              <Button onClick={() => "/review"}>GONE</Button>
             </Col>
 
           </div>
