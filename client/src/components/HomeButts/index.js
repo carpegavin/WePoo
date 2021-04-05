e79444fe6ee83dac8b87e6f7ce0a2daff805dde8
Gavin Phill <carpe.gavin@gmail.com>
2021-04-05 17:34:17 -0500

    made links into buttons

--- client/src/components/HomeButts/index.js
+++ client/src/components/HomeButts/index.js
@@ -2,32 +2,38 @@ import React, { Component } from "react";
 import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "./butts.css";
-import { props } from "react";
-// import './Navbar.css';
 import { Row, Container, Col, Span, ButtonGroup } from "react-bootstrap";
 // import { styles } from "react-bootstrap";
 // import ""{ useHistory } from 'react-router-dom';
-
+// import { props } from "react";
+// import './Navbar.css';
 // const history = useHistory();
 
 class HomeButton extends Component {
   render() {
     return (
-      <div className="container" style={{ textAlign: "center" }}>
-        <div class="btn-group btn-group-sm" vertical>
-          <div class="butts vertical-center">
+      <div className="container" id="linkStyle" style={{ textAlign: "center" }}>
+        <div className="btn-group btn-group-sm" vertical="true">
+          <div className="butts vertical-center">
 
             <Col style={{ height: "9.5rem" }}>
-              <Button onClick={()=> 
-                this.props.history.push('/map')}>GOTTA GO</Button>
+              <Link className="individualLinkerino" to="/map">
+                <p class="text">GOTTA GO</p>
+              </Link>
             </Col>
 
             <Col style={{ height: "9.5rem" }}>
-              <Button onClick={() => "/player"}>GOING</Button>
+              <Link className="individualLinkerino"
+              to="/player">
+              <p class="text">GOING</p>
+              </Link>
             </Col>
 
-            <Col style={{ height: "9.5rem" }}>
-              <Button onClick={() => "/review"}>GONE</Button>
+            <Col >
+              <Link className="individualLinkerino"
+              to="/review">
+              <p class="text">GONE</p>
+              </Link>
             </Col>
 
           </div>
