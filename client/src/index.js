80d562eaf9d49ce20f5ebda0f2773943207e7574
Devante W <williams.devante05@gmail.com>
2021-04-08 18:59:40 -0500

    added PWA features

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
 
-serviceWorkerRegistration.register();
